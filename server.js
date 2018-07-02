3f62d27907f0861b0bd8f7d8f28861da5848f75b
davified <davified@gmail.com>
2018-07-02 10:43:46 +0800

    Refactor CRUD handlers for /blogposts

--- server.js
+++ server.js
@@ -1,84 +1,42 @@
-// 1. SETTING UP THE DEPENDENCIES
-var express = require("express");
-var bodyParser = require("body-parser");
-var cors = require("cors");
-var port = process.env.PORT || 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+let blogposts = require("./seedData").blogposts;
+let users = require("./seedData").users;
 
-// 2. INSTANTIATING THE APP
-var app = express();
+const app = express();
 
-app.listen(port, function() {
-  console.log("your app is ready at port 3000!");
-});
-
-// mounting sub-app: view engine (ejs)
-app.set("view engine", "ejs");
+app.listen(PORT, function() {});
 
 app.get("/", function(req, res) {
   res.json({
-    0: "GET    /",
+    0: "GET       /",
     1: "-----------------------",
-    2: "GET    /blogposts",
-    3: "GET    /blogposts/:id",
-    4: "POST   /blogposts",
-    5: "PUT    /blogposts/:id",
+    2: "GET       /blogposts",
+    3: "GET       /blogposts/:id",
+    4: "POST      /blogposts",
+    5: "PUT       /blogposts/:id",
     6: "DELETE    /blogposts/:id",
     7: "-----------------------",
-    8: "GET    /users",
-    9: "GET    /users/:id",
-    10: "POST    /users",
-    11: "PUT    /users/:id",
-    12: "DELETE    /users/:id"
+    8: "GET       /users",
+    9: "GET       /users/:id",
+    10: "POST     /users",
+    11: "PUT      /users/:id",
+    12: "DELETE   /users/:id"
   });
 });
 
 app.use(cors());
-
-// 3.3 setting up body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// res.send('whatever'), res.render('ejs-filename'),
-// res.json({key: "value"}),
-
-var blogposts = [
-  {
-    id: 1,
-    title: "this is my FIRST blogpost",
-    body:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    author: "david",
-    likes: 20,
-    image_url: "https://static.pexels.com/photos/62418/pexels-photo-62418.jpeg"
-  },
-  {
-    id: 2,
-    title: "this is my SECOND blogpost",
-    body:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    author: "david",
-    likes: 15,
-    image_url:
-      "https://static.pexels.com/photos/200925/pexels-photo-200925.jpeg"
-  },
-  {
-    id: 3,
-    title: "this is my THIRD blogpost",
-    body:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    author: "jon",
-    likes: 200,
-    image_url: "https://static.pexels.com/photos/57840/pexels-photo-57840.jpeg"
-  }
-];
 
 app.get("/blogposts", function(req, res) {
   res.json(blogposts);
 });
 
 app.post("/blogposts", function(req, res) {
-  console.log(req.body);
-  var newPost = {
+  const newPost = {
     id: blogposts.length + 1,
     body: req.body.body,
     title: req.body.title,
@@ -91,58 +49,35 @@ app.post("/blogposts", function(req, res) {
 });
 
 app.get("/blogposts/:id", function(req, res) {
-  var id = parseInt(req.params.id, 10);
-  var requestedPost;
-  for (var i = 0; i < blogposts.length; i++) {
-    if (blogposts[i].id === id) {
-      requestedPost = blogposts[i];
-    }
-  }
+  const id = parseInt(req.params.id, 10);
+  const requestedPost = blogposts.find(blogpost => blogpost.id === id);
+
   res.json(requestedPost);
 });
 
 app.put("/blogposts/:id", function(req, res) {
-  // SAME CODE AS A GET /blogposts/:id REQUEST!
-  var id = parseInt(req.params.id, 10);
-  var requestedPost;
-  for (var i = 0; i < blogposts.length; i++) {
-    if (blogposts[i].id === id) {
-      requestedPost = blogposts[i];
-    }
-  }
+  const id = parseInt(req.params.id, 10);
+  const requestedPost = blogposts.find(blogpost => blogpost.id === id);
+  const updatedPost = Object.assign(requestedPost, req.body);
 
-  // this part is different. we extract the contents of the req.body object, and update the requestedPost object accordingly.
-  requestedPost.title = req.body.title;
-  requestedPost.body = req.body.body;
-
-  res.json(blogposts);
+  res.json(updatedPost);
 });
 
 app.delete("/blogposts/:id", function(req, res) {
-  var requestedId = parseInt(req.params.id, 10);
-  for (var i = 0; i < blogposts.length; i++) {
-    if (blogposts[i].id === requestedId) {
-      blogposts.splice(i, 1);
-    }
-  }
-  res.json(blogposts);
-});
+  const requestedId = parseInt(req.params.id, 10);
+  blogposts = blogposts.filter(blogpost => blogpost.id !== requestedId);
 
-var users = [
-  { id: 0, username: "david-tan", age: 28 },
-  { id: 1, username: "tang-wei", age: 50 },
-  { id: 2, username: "jonathan", age: 90 },
-  { id: 3, username: "yvonne", age: 20 }
-];
+  res.json({
+    message: `blogpost with id ${requestedId} deleted successfully.`
+  });
+});
 
 app.get("/users", function(req, res) {
   res.json(users);
 });
 
 app.post("/users", function(req, res) {
-  console.log(req);
-  console.log(req.body);
-  var newUser = {
+  const newUser = {
     id: users.length + 1,
     username: req.body.username,
     age: req.body.age
@@ -152,8 +87,8 @@ app.post("/users", function(req, res) {
 });
 
 app.get("/users/:id", function(req, res) {
-  var id = parseInt(req.params.id, 10);
-  var requestedUser;
+  const id = parseInt(req.params.id, 10);
+  let requestedUser;
   users.forEach(function(element) {
     if (element.id === id) {
       requestedUser = element;
@@ -164,8 +99,8 @@ app.get("/users/:id", function(req, res) {
 
 app.put("/users/:username", function(req, res) {
   // SAME CODE AS A GET /blogposts/:id REQUEST!
-  var requestedUser;
-  for (var i = 0; i < users.length; i++) {
+  let requestedUser;
+  for (let i = 0; i < users.length; i++) {
     if (users[i].username === req.params.username) {
       requestedUser = users[i];
     }
@@ -178,7 +113,7 @@ app.put("/users/:username", function(req, res) {
 });
 
 app.delete("/users/:username", function(req, res) {
-  for (var i = 0; i < users.length; i++) {
+  for (let i = 0; i < users.length; i++) {
     if (users[i].username === req.params.username) {
       users.splice(i, 1);
     }
