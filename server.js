56700183ea0c3fa5bcea746996401bf9975f8d6e
davified <davified@gmail.com>
2018-07-06 13:10:58 +0800

    Add /api-docs to landing page

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", function(req, res) {
   res.json({
-    0: "GET       /",
+    0: "GET       /api-docs",
     1: "-----------------------",
     2: "GET       /blogposts",
     3: "GET       /blogposts/:id",
